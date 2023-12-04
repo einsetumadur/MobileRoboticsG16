8121c032761adbd08982fd0c67f0e10b67b0641a
Tom_rathjens <tom.rathjens@epfl.ch>
2023-12-04 10:57:57 +0100

    tested local nav

--- src/Local_Nav/local_navigation.py
+++ src/Local_Nav/local_navigation.py
@@ -1,8 +1,9 @@
-from src.Local_Nav import psymap as pm
+#from src.Local_Nav import psymap as pm
 from src.Motion_Control import thymio as th
-async def local_navigation(rob,obstacles):
-    proximity_values = await th.get_proximity_values()
-    global state
+#async def local_navigation(rob,obstacles):
+async def local_navigation(client,node):
+    proximity_values = await th.get_proximity_values(client)
+    state=1
 
     w_l = [40,  20, -20, -20, -40,  30, -10, 8, 0]
     
@@ -14,13 +15,13 @@ async def local_navigation(rob,obstacles):
     x = [0,0,0,0,0,0,0,0,0]
     
     #Considering also the global obstacle as obstacles : 
-    x_glob=pm.hallucinate_map(rob,obstacles,img=None)
+    #x_glob=pm.hallucinate_map(rob,obstacles,img=None)
 
     if state != 0:
         for i in range(5):
             # Get and scale inputs
-            x[i] = (proximity_values[i] +x_glob[i])// sensor_scale
-
+            #x[i] = (proximity_values[i] +x_glob[i])// sensor_scale
+            x[i] = (proximity_values[i]//sensor_scale)
         y = [100,100]    
         
         for i in range(len(x)):    
@@ -32,4 +33,4 @@ async def local_navigation(rob,obstacles):
         y = [0,0] 
     
     # Set motor powers
-    await th.motorset(y[0],y[1])
+    await th.motorset(node,y[0],y[1])
