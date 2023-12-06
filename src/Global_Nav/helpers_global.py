28d1fdbb9dd9a8d0a09842d3b3acd3b3238e34e7
jdrouet27 <julien.drouet@epfl.ch>
2023-12-06 13:57:38 +0100

    new global path

--- src/Global_Nav/helpers_global.py
+++ src/Global_Nav/helpers_global.py
@@ -31,22 +31,9 @@ def global_final(occupancy_grid, start, goal, movement , plot=False):
         ax_astar.plot(path2[0], path2[1], marker="o", color = 'blue')
         ax_astar.scatter(start[0], start[1], marker="o", color = 'green', s=200)
         ax_astar.scatter(goal[0], goal[1], marker="o", color = 'purple', s=200)
-    return changement_direction(path)
+    return compute_change(path)
     
     
-
-# give the next local goal for a position on the path
-def next_checkpoint(path, position):
-    for i in range(len(path) - 1):
-        x1, y1 = path[i]
-        x2, y2 = path[i+1]
-
-       
-        if (x1-0.8<= position[0] <= x2+0.8 or x1+0.8>= position[0] >= x2-0.8) and \
-           (y1-0.8<= position[1] <= y2+0.8 or y1+0.8>= position[1] >= y2-0.8):
-            return path[i+1]
-
-    return path[i]
 
 def next_checkpoint2(path, position, counter,local_obstacle):
     if counter< len(path) -1:
@@ -104,7 +91,7 @@ def test_if_goal(goal, position_robot):
 
 ## Inutile 
 
-def changement_direction(path):
+def compute_change(path):
     if len(path) < 2:
         return []
 
@@ -166,7 +153,6 @@ def reconstruct_path(cameFrom, current):
     """
     total_path = [current]
     while current in cameFrom.keys():
-        # Add where the current node came from to the start of the list
         total_path.insert(0, cameFrom[current]) 
         current=cameFrom[current]
     return total_path
@@ -182,23 +168,17 @@ def A_Star(start, goal, h, coords, occupancy_grid, movement_type="4N"):
     :return: a tuple that contains: (the resulting path in meters, the resulting path in data array indices)
     """
     
-    # -----------------------------------------
-    # DO NOT EDIT THIS PORTION OF CODE
-    # -----------------------------------------
     
-    # Check if the start and goal are within the boundaries of the map
     for point in [start, goal]:
         assert point[0]>=0 and point[0]<occupancy_grid.shape[0], "start or end goal not contained in the map"
         assert point[1]>=0 and point[1]<occupancy_grid.shape[1], "start or end goal not contained in the map"
     
-    # check if start and goal nodes correspond to free spaces
     if occupancy_grid[start[0], start[1]]:
         raise Exception('Start node is not traversable')
 
     if occupancy_grid[goal[0], goal[1]]:
         raise Exception('Goal node is not traversable')
     
-    # get the possible movements corresponding to the selected connectivity
     if movement_type == '4N':
         movements = _get_movements_4n()
     elif movement_type == '8N':
@@ -207,66 +187,53 @@ def A_Star(start, goal, h, coords, occupancy_grid, movement_type="4N"):
         raise ValueError('Unknown movement')
     
 
-    # The set of visited nodes that need to be (re-)expanded, i.e. for which the neighbors need to be explored
-    # Initially, only the start node is known.
     openSet = [start]
     
-    # The set of visited nodes that no longer need to be expanded.
     closedSet = []
 
-    # For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start to n currently known.
     cameFrom = dict()
 
-    # For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
     gScore = dict(zip(coords, [np.inf for x in range(len(coords))]))
     gScore[start] = 0
 
-    # For node n, fScore[n] := gScore[n] + h(n). map with default value of Infinity
     fScore = dict(zip(coords, [np.inf for x in range(len(coords))]))
     fScore[start] = h[start]
 
-    # while there are still elements to investigate
     while openSet != []:
         
-        #the node in openSet having the lowest fScore[] value
         fScore_openSet = {key:val for (key,val) in fScore.items() if key in openSet}
         current = min(fScore_openSet, key=fScore_openSet.get)
         del fScore_openSet
         
-        #If the goal is reached, reconstruct and return the obtained path
         if current == goal:
             return reconstruct_path(cameFrom, current), closedSet
 
         openSet.remove(current)
         closedSet.append(current)
         
-        #for each neighbor of current:
         for dx, dy, deltacost in movements:
             
             neighbor = (current[0]+dx, current[1]+dy)
             
-            # if the node is not in the map, skip
+
             if (neighbor[0] >= occupancy_grid.shape[0]) or (neighbor[1] >= occupancy_grid.shape[1]) or (neighbor[0] < 0) or (neighbor[1] < 0):
                 continue
             
-            # if the node is occupied or has already been visited, skip
             if (occupancy_grid[neighbor[0], neighbor[1]]) or (neighbor in closedSet): 
                 continue
                 
-            # d(current,neighbor) is the weight of the edge from current to neighbor
-            # tentative_gScore is the distance from start to the neighbor through current
-            tentative_gScore = gScore[current] + deltacost
+            
+            safety_margin = calculate_safety_margin(neighbor, occupancy_grid)
+            tentative_gScore = gScore[current] + deltacost*safety_margin
             
             if neighbor not in openSet:
                 openSet.append(neighbor)
                 
             if tentative_gScore < gScore[neighbor]:
-                # This path to neighbor is better than any previous one. Record it!
                 cameFrom[neighbor] = current
                 gScore[neighbor] = tentative_gScore
                 fScore[neighbor] = gScore[neighbor] + h[neighbor]
 
-    # Open set is empty but goal was never reached
     print("No path found to goal")
     return [], closedSet
 
@@ -274,16 +241,14 @@ def A_Star(start, goal, h, coords, occupancy_grid, movement_type="4N"):
 
 
 def get_path(occupancy_grid,start, goal, movement):
-    # start : position of the robot
-    # goal : always the same
-    # grid : grid with obstacles and 
+    
     x,y = np.mgrid[0:occupancy_grid.shape[0]:1, 0:occupancy_grid.shape[1]:1]
     pos = np.empty(x.shape + (2,))
     pos[:, :, 0] = x; pos[:, :, 1] = y
     pos = np.reshape(pos, (x.shape[0]*x.shape[1], 2))
     coords = list([(int(x[0]), int(x[1])) for x in pos])
 
-    # Define the heuristic, here = distance to goal ignoring obstacles
+    
     h = np.linalg.norm(pos - goal, axis=-1)
     h = dict(zip(coords, h))
 
@@ -323,7 +288,6 @@ def douglas_peucker(coords, epsilon):
     if len(coords) <= 2:
         return [coords[0], coords[-1]]
 
-    # Find the point with the maximum distance
     dmax = 0
     index = 0
     end = len(coords) - 1
@@ -333,15 +297,12 @@ def douglas_peucker(coords, epsilon):
             index = i
             dmax = d
 
-    # If max distance is greater than epsilon, recursively simplify
     if dmax > epsilon:
         results1 = douglas_peucker(coords[:index + 1], epsilon)
         results2 = douglas_peucker(coords[index:], epsilon)
 
-        # Combine the results
         results = results1[:-1] + results2
     else:
-        # Otherwise, keep the endpoints
         results = [coords[0], coords[end]]
 
     return results
@@ -352,3 +313,28 @@ def point_to_line_distance(point, start, end):
     denominator = ((end[1] - start[1]) ** 2 + (end[0] - start[0]) ** 2) ** 0.5
     distance = numerator / denominator if denominator != 0 else 0
     return distance
+
+def calculate_safety_margin(neighbor, occupancy_grid):
+    distance_threshold_big = 5  
+    distance_threshold_small = 3
+    distance_to_obstacle = distance_to_nearest_obstacle(neighbor, occupancy_grid)
+
+    if distance_to_obstacle < distance_threshold_big:
+        if distance_to_obstacle < distance_threshold_small:
+            safety_margin = 2     
+        else: 
+            safety_margin = 1.5  
+    else:
+        safety_margin = 1.0  
+
+    return safety_margin
+
+
+
+
+def distance_to_nearest_obstacle(point, occupancy_grid):
+   
+    distances = np.linalg.norm(np.array(np.where(occupancy_grid == 1)).T - np.array(point), axis=1)
+    min_distance = np.min(distances)
+
+    return min_distance
