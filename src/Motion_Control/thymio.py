6510adb36c55e76a0b69c6ce2d4b04f5bf720ddf
Tom_rathjens <tom.rathjens@epfl.ch>
2023-12-07 12:19:39 +0100

    implemented kidnapping function

--- src/Motion_Control/thymio.py
+++ src/Motion_Control/thymio.py
@@ -53,9 +53,16 @@ async def get_proximity_values(client):
     await node.wait_for_variables({"prox.horizontal"})
     # Get the proximity values : v: Stands for "variables" and is used to access the cached variable values.
     proximity_values = node.v.prox.horizontal
-    # Return the value of the front proximity sensor (index 2)
     return proximity_values[0:5]
 
+async def get_proximity_ground_values(client):
+    # Wait for the Thymio node
+    node = await client.wait_for_node()
+    # Wait for the proximity sensor variables
+    await node.wait_for_variables({"prox.ground.delta"})
+    # Get the proximity values : v: Stands for "variables" and is used to access the cached variable values.
+    proximity_ground_values = node.v.prox.ground
+    return proximity_ground_values[0:2]
 
 
 
